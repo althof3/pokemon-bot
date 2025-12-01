e19c7c67212e42be25b8a94da37c73ccc56ff00f
althof3 <althoframdhan3@gmail.com>
2025-12-02 00:14:05 +0700

    hah

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -7,15 +7,6 @@ export class UserService {
   async register(payload: {name: string, phone: string}): Promise<User & { isNew: boolean}> {
     console.log('[register user]', payload);
     
-    const existing = await this.userRepo.findOneBy({ phone: payload.phone });
-    if (existing) {
-      const now = new Date();
-      await this.userRepo.update(existing.id, {
-        updatedAt: now,
-      });
-      return {...existing, updatedAt: now, isNew: false};
-    }
-
     const createdUser = await this.userRepo.save({
       ...payload,
       loginAt: new Date(),
